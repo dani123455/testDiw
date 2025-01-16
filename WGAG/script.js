08c5c93766a1458cd8743baa4f9217ec42e54134
Daniel <daniel.mg1705@gmail.com>
2025-01-16 14:17:55 +0100

    terminar lectura

--- WGAG/script.js
+++ WGAG/script.js
@@ -7,7 +7,13 @@ let contador = 0;
 let contador2 = 0;
 const progreso = document.getElementById('progreso');
 const progreso2 = document.getElementById('progreso%');
-
+const hora = new Date().getHours();
+const saludo = document.getElementById('Bienvenida');
+const texto = document.getElementById('texto');
+const body = document.getElementById('body');
+let px = 16;
+let estaAmpliado = false;
+const botonArriba = document.getElementById('botonArriba');
 
 
 
@@ -43,8 +49,6 @@ if (botonFondo) {
 }
 
 
-
-
 //contador de cliks
 click.addEventListener('click', function(){
     contador++;
@@ -53,20 +57,71 @@ click.addEventListener('click', function(){
 
 
 //Barra progreso 
-
 window.addEventListener('scroll', function() {
-    let scroll = document.documentElement.scrollTop; 
-    let scrollAbajo = document.documentElement.scrollHeight; 
+    let scroll = window.scrollY; 
+    let scrollAbajo = document.documentElement.scrollHeight - window.innerHeight; 
+    let porcentaje = (scroll / scrollAbajo) * 100;
 
-    
-    if (scroll < scrollAbajo) {
-        if (contador2 < 100) { 
-            contador2++;
-            progreso.value = contador2; 
-            progreso2.innerHTML = contador2 + '%';
-        }
+    if (contador2 < 100) {
+        contador2 = Math.floor(porcentaje); 
+        progreso.value = contador2; 
+        progreso2.innerHTML = contador2 + '%';
+    } else{
+        contador2 = Math.max(100 - Math.floor(porcentaje));
+    }
+    //codigo para el boton para subir
+    if (contador2 > 50) {
+        containerBotonArriba.style.display = 'block'; 
+    } else {
+        containerBotonArriba.style.display = 'none'; 
     }
 });
 
+//Dar la bienvenida
 
-//Arreglar progreso añadir boton de ir arriba , dar bienvenida , leer pagina y aumentar texto 
+function bienvenida(){
+    if (hora>=6 && hora<12){
+        saludo.textContent = 'Buenos Dias!!!'
+    } else if (hora>=12 && hora<20){
+        saludo.textContent = 'Buenas Tardes!!!'
+    } else{
+        saludo.textContent = 'Buenas Noches!!!'
+    }
+}
+
+
+
+//Aumentar texto
+    texto.addEventListener('click', function(){
+        if (estaAmpliado) {
+            px = 16; 
+            estaAmpliado = false; 
+        } else {
+            px = px+4;
+            estaAmpliado = true; 
+        }
+        body.style.fontSize = `${px}px`; 
+    });
+    
+
+
+//Boton arriba
+botonArriba.addEventListener('click', function() {
+    body.scrollIntoView({ behavior: 'smooth' });
+});
+
+bienvenida();
+
+//lectura de pagina
+
+document.getElementById('lectura').addEventListener('click', function() {
+    const texto = document.body.innerText;
+    const utterance = new SpeechSynthesisUtterance(texto);
+
+    utterance.lang = 'es-ES'; 
+    utterance.rate = 1; 
+    utterance.pitch = 1; 
+
+    
+    window.speechSynthesis.speak(utterance);
+});
